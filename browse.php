4de0963ad9cf5f83a1fdaffaa9dbcae4e10f8a92
jcante11 <juan_cante@uri.edu>
2024-04-14 15:17:04 -0400

    added the functions from toystore database but not working 100%

---
+++ browse.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'includes/database-connection.php'; 
+
+echo "helloworld";
--- login.php
+++ login.php
@@ -3,6 +3,34 @@
 // Require the database connection file
 require 'includes/database-connection.php'; 
 
+function get_cust_info(PDO $pdo, string $id) {
+
+    // SQL query to retrieve toy information based on the toy ID
+    $sql = "SELECT email
+        FROM Customers
+        WHERE email= :id;";	// :id is a placeholder for value provided later 
+                                   // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
+
+
+    // Execute the SQL query using the pdo function and fetch the result
+    $credentials = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+
+    // Return the toy information (associative array)
+    return $credentials;
+}
+
+// Check if the request method is POST (i.e, form submitted)
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Retrieve the value of the 'email' field from the POST data
+    $uname = $_POST['uname'];
+
+    // Retrieve the value of the 'orderNum' field from the POST data
+    $password = $_POST['password'];
+
+    $cust_info = get_cust_info($pdo, $uname);
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +42,9 @@ require 'includes/database-connection.php';
 </head>
 
 <body>
-    <form action="" method="post">              <!-- action goes to next page -->
+    <form>
         <h2>Login</h2>
+
         <label>Username</label>
         <input type="text" name="uname" placeholder="Username"><br>
 
