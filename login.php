42824dff150558434b3e6468e2e37cdb7e352b7f
jcante11 <juan_cante@uri.edu>
2024-04-15 20:11:22 -0400

    fixed error message

--- login.php
+++ login.php
@@ -1,51 +1,25 @@
 <?php
 
-// Require the database connection file
-require 'includes/database-connection.php';
+    // Require the database connection file
+    require 'includes/database-connection.php';
 
-$error = '';
+    function get_cust_info(PDO $pdo, string $id) {
 
-function get_cust_info(PDO $pdo, string $id) {
-
-    // SQL query to retrieve username and password from the database
-    $sql = "SELECT email, password 
+        // SQL query to retrieve username and password from the database
+        $sql = "SELECT email, password 
             FROM Customers WHERE email = :id";		// Select the email and password from the customer table where the email is equal to the value of :id
 
 
-    // Execute the SQL query using the pdo function and fetch the result
-    $credentials = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
+        // Execute the SQL query using the pdo function and fetch the result
+        $credentials = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
 
-    // Return the toy information (associative array)
-    return $credentials;
-}
+        // Return the toy information (associative array)
+        return $credentials;
+    }
 
 ?>
 
-<!DOCTYPE html>
-
-<html>
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-
-<body>
-    <form method="POST">
-        <h2>Login</h2>
-
-        <label>Username</label>
-        <input type="text" name="uname" placeholder="Username"><br>
-
-        <label>Password</label>
-        <input type="password" name="password" placeholder="Password"><br>
-
-        <button type="submit">Login</button>
-
-        <p>Don't have an account? <a href="signup.php">Sign Up</a></p>
-        
-    </form>
-
-    <?php
+<?php
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
@@ -61,12 +35,45 @@ function get_cust_info(PDO $pdo, string $id) {
 
                 // Redirect to the browse page
                 header("Location: http://localhost:80/streaming_database/browse.php");
-            } else {
+                exit();
+            }
+
+            else {
                 // Display an error message
-                echo "Invalid username or password";
+                header("Location: login.php?error=Invalid Username or Password");
+                exit();
             }
         }
     ?>
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+    <form method="POST">
+        <h2>Login</h2>
+
+        <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?> </p>
+            <?php } ?>
+
+        <label>Username</label>
+        <input type="text" name="uname" placeholder="Username"><br>
+
+        <label>Password</label>
+        <input type="password" name="password" placeholder="Password"><br>
+
+        <button type="submit">Login</button>
+
+        <p>Don't have an account? <a href="signup.php">Sign Up</a></p>
+        
+    </form>
     
 </body>
 
